584901557c252e09d26b4506edabf5d5810c9937
Peteski <38994044+YAOS5@users.noreply.github.com>
2020-09-15 19:13:20 +1000

    Download data with urllib3

--- generate_data.py
+++ generate_data.py
@@ -1,10 +1,16 @@
-from alpha_vantage.timeseries import TimeSeries
+# from alpha_vantage.timeseries import TimeSeries
 
-api_key = "VHY92LCH1E6NU73I"
+# function="TIME_SERIES_INTRADAY_EXTENDED"
+# api_key = "VHY92LCH1E6NU73I"
 
-ts = TimeSeries(key=api_key, output_format="pandas")
-data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full')
-print(data)
+# ts = TimeSeries(key=api_key, output_format="pandas")
+# data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full', slice="year1month12")
+# print(data)
 
 
-
+import urllib3, shutil
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=demo"
+c = urllib3.PoolManager()
+filename = "test.csv"
+with c.request('GET', url, preload_content=False) as res, open(filename, 'wb') as out_file:
+    shutil.copyfileobj(res, out_file)
