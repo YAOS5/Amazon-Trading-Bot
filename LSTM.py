f5ac7bc54ff06129b421cd1817f5d8bf45f4564d
Goolace <eushuen16@gmail.com>
2020-10-29 20:32:24 +1100

    updated plots in report with LSTM

--- LSTM.py
+++ LSTM.py
@@ -1,5 +1,5 @@
 '''
-    Script trains LSTM on training data.
+    Script trains LSTM on 3-month window of test data, and predicts the stock prices of the following month
     Generates LSTM_portfolio_value.csv which records the portfolio values of LSTM trader on testing data.
 '''
 # with transaction cost included
@@ -11,6 +11,9 @@ from sklearn.preprocessing import MinMaxScaler
 
 INITIAL_BALANCE = 10_000
 SELL, HOLD, BUY = 0, 1, 2
+EPOCHS = 20
+BATCH_SIZE = 72
+transaction_cost = 0.001
 
 def create_model():
     model = Sequential()
@@ -19,14 +22,13 @@ def create_model():
     model.add(Dropout(0.4))
     model.add(Dense(1))
     ADAM = Adam(0.00001, beta_1=0.9, beta_2=0.999, amsgrad=False)
-    model.compile(loss='mean_squared_error', optimizer=ADAM)
-    
+    model.compile(loss='mean_squared_error', optimizer=ADAM) 
     return model
 
-
-transaction_cost = 0.001
 def take_action(action, curr_position, curr_price, balance):
-    
+    '''
+        Calculates the change in balance due to change in position
+    '''
     # Perform position transition (transaction cost is a proportion of price)
     balance -= curr_price * transaction_cost * abs(action - curr_position)
 
@@ -44,70 +46,72 @@ def take_action(action, curr_position, curr_price, balance):
     
     return balance
 
+
 actions = []
 portfolio_values = [INITIAL_BALANCE]
 balance = INITIAL_BALANCE
 prices = []
 prev_action = HOLD
-
 for i in range(6):
-    
+
     data = load_data([i for i in range(6-i,6-i+4)])
-    
+
     dataset = np.reshape(data['close'].values,(-1,1))
-    
+
     scaled_dataset = dataset
     train= scaled_dataset[:int(scaled_dataset.shape[0]*0.75)]
     valid = scaled_dataset[int(scaled_dataset.shape[0]*0.75)-60:]
-    
+
     sc = MinMaxScaler(feature_range = (0, 1))
     train2 = sc.fit_transform(train)
     valid2 = sc.transform(valid)
     x_train, y_train, x_test, y_test = [], [], [], []
-    
-    for j in range(60,train2.shape[0]):
+
+    for j in range(60, train2.shape[0]):
         x_train.append(train2[j-60:j,0])
         y_train.append(train2[j,0])
-        
-    for z in range(60,valid2.shape[0]):
+
+    for z in range(60, valid2.shape[0]):
         x_test.append(valid2[z-60:z,0])
         y_test.append(valid2[z,0])
-    
+
     x_train, y_train, x_test, y_test = np.array(x_train), np.array(y_train), np.array(x_test), np.array(y_test)
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1],1))
 
-    model = tf.keras.models.load_model(f'./extras/LSTM_checkpoints/{i}')
-    
+    model = create_model()
+    history = model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(x_test, y_test),
+                        verbose=1, shuffle=False, workers=-1)
+    #model = tf.keras.models.load_model(f'./extras/LSTM_checkpoints/{i}')
+    #model.save(f"./extras/LSTM_checkpoints/{i})
+
     predicted_stock_price = sc.inverse_transform(model.predict(x_test))
     actual_stock_price = sc.inverse_transform(y_test.reshape((-1, 1)))
-    
+
     for j in range(len(actual_stock_price)):
         prices.append(actual_stock_price[j][0])
-    
-    
+
     for j in range(1, len(predicted_stock_price)):
         buy_cost = actual_stock_price[j-1] * transaction_cost * abs(BUY - prev_action)
         sell_cost = actual_stock_price[j-1] * transaction_cost * abs(SELL - prev_action)
-        
-        # target action : BUY
+
+        # BUY
         if predicted_stock_price[j] - actual_stock_price[j-1] > buy_cost:
             action = BUY
-            actions.append(BUY)
-             
-        # target action : SELL
+
+        # SELL
         elif actual_stock_price[j-1] - predicted_stock_price[j] > sell_cost:
             action = SELL
-            actions.append(SELL)
-            
-        # target action: HOLD
+
+        # HOLD
         else:
-            action = HOLD
-            actions.append(HOLD)
-        
+            action = prev_action
+
+        actions.append(action)
+
         # take action
         balance = take_action(action, prev_action, actual_stock_price[j-1], balance)
-        
+
         # reward
         if action == BUY:
             portfolio_value = balance + actual_stock_price[j]
@@ -115,12 +119,13 @@ for i in range(6):
             portfolio_value = balance - actual_stock_price[j]
         else:
             portfolio_value = balance
-            
+
         portfolio_values.append(float(portfolio_value))
         prev_action = action
-        
+
 # plot(prices=prices,target_positions = actions,portfolio_values = portfolio_values)
-    
-    
+
 df = pd.DataFrame(portfolio_values)
-df.to_csv("LSTM_portfolio_values", header=None)
+# df.to_csv("LSTM_portfolio_values", header=None)
+
+    
